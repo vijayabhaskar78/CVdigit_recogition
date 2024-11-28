3300177ce3b1f71c2af0b73004970bbf7f68cdc0
vijayabhaskar78 <123809324+vijayabhaskar78@users.noreply.github.com>
2024-11-28 14:41:24 +0530

    Update basic_knn.py

--- basic_knn.py
+++ basic_knn.py
@@ -2,43 +2,38 @@ import numpy as np
 import cv2
 
 # Loading the digits data
-data = cv2.imread('images/digits.png')
+data = cv2.imread('images/digits.png', cv2.IMREAD_COLOR)
+if data is None:
+    raise FileNotFoundError("The file 'images/digits.png' was not found. Please check the path.")
 gray = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
 
-# Resizing each digit from 20x20 to 10x10
+# Resizing for visualization
 resized = cv2.pyrDown(gray)
+cv2.imshow("Original Data", resized)
+cv2.waitKey(500)
+cv2.destroyAllWindows()
 
-cv2.imshow("Orignal Data", resized)
-
-# Splitting image original image into 5000 different arrays of size 20x20
-# Resulting array: 50 * 100 * 20 * 20
-
-arr = [np.hsplit(i, 100) for i in np.vsplit(gray, 50)]
+# Splitting the image into 5000 sub-images of size 20x20
+arr = [np.hsplit(row, 100) for row in np.vsplit(gray, 50)]
 arr = np.array(arr)
-print ("Resulting Shape", arr.shape)
+print("Resulting Shape", arr.shape)
 
-# Spliting into training and test set
-# Total: 5000, Train: 3500 images, Test: 1500
-X_train = arr[:, :70].reshape(-1, 400).astype(np.float32)
-X_test = arr[:, 70:100].reshape(-1, 400).astype(np.float32)
-print ("input shapes\n-->Train: {}, Test: {}".format(X_train.shape, X_test.shape))
+# Splitting into training and test sets
+X_train = arr[:, :70].reshape(-1, 400).astype(np.float32)  # Train: 70 columns
+X_test = arr[:, 70:].reshape(-1, 400).astype(np.float32)   # Test: 30 columns
+print("Input shapes\n--> Train: {}, Test: {}".format(X_train.shape, X_test.shape))
 
-## Targets for each image
-y = [0,1,2,3,4,5,6,7,8,9]
+# Targets for each image
+y = np.arange(10)  # 0 to 9
+y_train = np.repeat(y, 350)[:, np.newaxis]  # 70% for training
+y_test = np.repeat(y, 150)[:, np.newaxis]   # 30% for testing
+print("Target shapes\n--> Train: {}, Test: {}".format(y_train.shape, y_test.shape))
 
-y_train = np.repeat(y, 350)[:, np.newaxis]
-y_test = np.repeat(y, 150)[:, np.newaxis] 
-print ("target shapes\n-->Train: {}, Test: {}".format(y_train.shape, y_test.shape))
-
-# Using K-NN(k- nearest neighbors) as the ML algorithm
+# Using K-NN (k-nearest neighbors)
 classifier_knn = cv2.ml.KNearest_create()
 classifier_knn.train(X_train, cv2.ml.ROW_SAMPLE, y_train)
-response, result, neighbours, distance = classifier_knn.findNearest(X_test, k=3)
 
-# Testing and calculating the accuracy of knn classifier
-correct = result == y_test
-correct = np.count_nonzero(correct)
-accuracy = correct * (100.0/result.size)
-print ("Accuracy: ", accuracy)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Testing and calculating accuracy
+ret, result, neighbours, distance = classifier_knn.findNearest(X_test, k=3)
+accuracy = np.mean(result == y_test) * 100
+print("Accuracy:", accuracy)
